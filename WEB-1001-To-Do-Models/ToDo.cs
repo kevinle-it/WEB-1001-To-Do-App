300315a016b16ecbb6936fc6a737f1930eb7f638
Tri Le <trile95.it@gmail.com>
2022-04-08 19:04:59 -0400

    add todo model

---
+++ WEB-1001-To-Do-Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB_1001_To_Do_Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
