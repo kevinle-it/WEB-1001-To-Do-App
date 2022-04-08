cb95021f9b304b57c4f470f1f9befb3c5e014b35
Tri Le <trile95.it@gmail.com>
2022-04-08 19:12:50 -0400

    add to do db context

---
+++ WEB-1001-To-Do-DB-Context/ToDoDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WEB_1001_To_Do_Models;
+
+namespace WEB_1001_To_Do_DB_Context
+{
+    public class ToDoDBContext : DbContext
+    {
+        public ToDoDBContext(DbContextOptions<ToDoDBContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
